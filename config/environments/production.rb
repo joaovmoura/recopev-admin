e1ca92117dcffacfba3ca445d1790ae20d2aee5c
joaovmoura <joao.vitor.figueiredo@ccc.ufcg.edu.br>
2026-03-21 21:58:36 -0300

    fix: configure default_url_options for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,8 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: ENV.fetch("APP_HOST", "192.168.0.10"), port: ENV.fetch("APP_PORT", 3001) }
+  routes.default_url_options = { host: ENV.fetch("APP_HOST", "192.168.0.10"), port: ENV.fetch("APP_PORT", 3001) }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
